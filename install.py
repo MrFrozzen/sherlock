4e5f7a5f85fa3e6b5f8479d30e92bed11897cb44
Dmitry <frozzenmr@ya.ru>
2021-01-15 02:46:14 +0300

    Add files via upload

--- install.py
+++ install.py
@@ -1,13 +1,30 @@
-import os
-print("Обновление pip...")
-os.system("pip3 install --upgrade pip")
-os.system("clear")
-print("              [Installer]\n")
-mod = ["html2text", "requests", "bs4", "vk_api", "urllib3"]
-print("Для работы этой версии шерлока, нужен доступ к хранилищу.")
-input("Нажмите Enter, для установки нужных библиотек.")
-os.system("termux-setup-storage")
-print("Начало установки.")
-for i in range(len(mod)):
-    print("Установка "+mod[i]+"...")
-    os.system("pip3 install "+mod[i])
+import os, sys
+while True:
+    if sys.argv[1]=='up':
+        t='0'
+    else:
+        t='1'
+        
+    if t == '1':
+        os.system("pip install requests")
+        os.system("pip install colorama")
+        os.system("pkg install php")
+        save_file = open("s.sh", "w+")
+        save_file.write("python3 sherl.py")
+        save_file.close()
+        os.system("cat s.sh > /data/data/com.termux/files/usr/bin/s")
+        os.system("chmod 700 /data/data/com.termux/files/usr/bin/s")
+        os.system("s")
+        break
+    elif t == '0':
+        os.system("pkg install wget")
+        os.system("rm sherl.py")
+        os.system("rm index.php")
+        os.system("rm i.tl")
+        os.system("wget https://raw.githubusercontent.com/termux-lab/sherlock/master/i.tl")
+        os.system("wget https://raw.githubusercontent.com/termux-lab/sherlock/master/sherl.py")
+        os.system("wget https://raw.githubusercontent.com/termux-lab/sherlock/master/index.php")
+        os.system("s")
+        break
+    else:
+        print("0/1 ???")
